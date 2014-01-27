f6c5d73d4ea69742cd2cdf261e0fef3d0e1ffe08
Sebastian Stöhr <sdstoehr@gmail.com>
2014-01-27 14:24:45 +0100

    added http status

--- src/main/java/de/sstoehr/harreader/model/HttpStatus.java
+++ src/main/java/de/sstoehr/harreader/model/HttpStatus.java
@@ -2,11 +2,23 @@ package de.sstoehr.harreader.model;
 
 public enum HttpStatus {
 
-    OK(200, "OK"), NO_CONTENT(204, "No Content"),
+    OK(200, "OK"), CREATED(201, "Created"), ACCEPTED(202, "Accepted"), NO_CONTENT(204, "No Content"), RESET_CONTENT(205, "Reset Content"),
+    PARTIAL_CONTENT(206, "Partial Content"),
 
-    MOVED_PERMANENTLY(301, "Moved Permanently"), FOUND(302, "Found"), SEE_OTHER(303, "See Other"), NOT_MODIFIED(304, "Not Modified");
+    MOVED_PERMANENTLY(301, "Moved Permanently"), FOUND(302, "Found"), SEE_OTHER(303, "See Other"), NOT_MODIFIED(304, "Not Modified"),
+    USE_PROXY(305, "Use Proxy"), TEMPORARY_REDIRECT(307, "Temporary Redirect"),
 
+    BAD_REQUEST(400, "Bad Request"), UNAUTHORIZED(401, "Unauthorized"), PAYMENT_REQUIRED(402, "Payment Required"), FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"), METHOD_NOT_ALLOWED(405, "Method Not Allowed"), NOT_ACCEPTABLE(406, "Not Acceptable"),
+    PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"), REQUEST_TIMEOUT(408, "Request Timeout"),
+    CONFLICT(409, "Conflict"), GONE(410, "Gone"), LENGTH_REQUIRED(411, "Length Required"), PRECONDITION_FAILED(412, "Precondition Failed"),
+    REQUEST_ENTITY_TOO_LARGE(413, "Request Entity Too Large"), REQUEST_URI_TOO_LONG(414, "Request-URI Too Long"),
+    UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"), REQUESTED_RANGE_NOT_SATISFIABLE(416, "Requested Range Not Satisfiable"),
+    EXPECTATION_FAILED(417, "Expectation Failed"),
 
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"), NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"), SERVICE_UNAVAILABLE(503, "Service Unavailable"), GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+    HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
 
     private int code;
     private String text;
@@ -34,6 +46,9 @@ public enum HttpStatus {
     }
 
     public static HttpStatus getByText(String text) {
+        if (text.equalsIgnoreCase("Moved Temporarily")) {
+            return HttpStatus.FOUND;
+        }
         for (HttpStatus httpStatus : HttpStatus.values()) {
             if (httpStatus.getText().equalsIgnoreCase(text)) {
                 return httpStatus;
---
+++ src/test/java/de/sstoehr/harreader/model/HttpStatusTest.java
@@ -0,0 +1,24 @@
+package de.sstoehr.harreader.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HttpStatusTest {
+
+    @Test
+    public void test302() {
+        Assert.assertEquals(HttpStatus.FOUND, HttpStatus.getByCode(302));
+        Assert.assertEquals(HttpStatus.FOUND, HttpStatus.getByText("fOUnd"));
+        Assert.assertEquals(HttpStatus.FOUND, HttpStatus.getByText("Moved temporarily"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidCode() {
+        HttpStatus.getByCode(1000);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidText() {
+        HttpStatus.getByText("Not a HTTP status");
+    }
+}
