d9ff74a0e7a4b5810da2b2bf182134a3305631fa
Jordan Gwyn <emailme.jtg+github@gmail.com>
2016-11-13 13:40:17 -0800

    add CCM_POST http method to enum
    https://github.com/sdstoehr/har-reader/issues/13

--- src/main/java/de/sstoehr/harreader/model/HttpMethod.java
+++ src/main/java/de/sstoehr/harreader/model/HttpMethod.java
@@ -1,5 +1,5 @@
 package de.sstoehr.harreader.model;
 
 public enum HttpMethod {
-    GET, POST, PUT, HEAD, PROPFIND, OPTIONS, REPORT, DELETE, CONNECT, TRACE;
+    GET, POST, PUT, HEAD, PROPFIND, OPTIONS, REPORT, DELETE, CONNECT, TRACE, CCM_POST;
 }
