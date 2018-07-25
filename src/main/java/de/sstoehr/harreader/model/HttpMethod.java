588335e2e176d64360fa3e4cba20e08db9ccf5f2
Alexander Bogdanov <abogdanov@gonitro.com>
2018-07-25 15:13:43 +0100

    Added PATCH to HttpMethod

--- src/main/java/de/sstoehr/harreader/model/HttpMethod.java
+++ src/main/java/de/sstoehr/harreader/model/HttpMethod.java
@@ -1,5 +1,5 @@
 package de.sstoehr.harreader.model;
 
 public enum HttpMethod {
-    GET, POST, PUT, HEAD, PROPFIND, OPTIONS, REPORT, DELETE, CONNECT, TRACE, CCM_POST;
+    GET, POST, PUT, HEAD, PROPFIND, OPTIONS, REPORT, DELETE, CONNECT, TRACE, CCM_POST, PATCH;
 }
